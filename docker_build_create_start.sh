30338476b151fad1296a947f25487db7cd6a73ee
Paul Thomson <paul.thomson11@imperial.ac.uk>
2017-03-27 19:13:00 +0100

    Small fix to ci.

--- docker_build_create_start.sh
+++ docker_build_create_start.sh
@@ -12,5 +12,5 @@ docker stop ${CONTAINER} || true
 docker rm ${CONTAINER} || true
 
 docker create -e CI_BUILD_REF -e GITHUB_TOKEN -ti --name ${CONTAINER} ${IMAGE}
-docker start -ai ${CONTAINER} | cat
+docker start -a ${CONTAINER} | cat
 
